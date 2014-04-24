6ad35b8a458dc728c532bf150adb6a7c18b89634
Sean <s.cunningham777@gmail.com>
2014-04-23 22:44:48 -0500

    Adding some skeleton files; minor update to express root file.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 app.get('/', function(req, res){
   res.send('index.html');
 });
+app.get('/partials/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('views/' + name);
+});
 
 var server = app.listen(8888, function() {
     console.log('Listening on port %d', server.address().port);
